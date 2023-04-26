d4f1b2c4b03da6b961745468e08ede46f3a51dc8
bahnisikha <bahnisikhadhar@gmail.com>
2023-04-26 08:09:29 +0530

    Add Todo Modal on off added

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -5,7 +5,7 @@ function Todo() {
   const [inputArr, setInputArr] = useState([]);
   const [toggleEdit, settoggleEdit] = useState(false);
   const [editIndex, setEditIndex] = useState(null);
-
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
   useEffect(() => {
     const storedInputArr = JSON.parse(localStorage.getItem('inputDetailItems'));
@@ -15,6 +15,10 @@ function Todo() {
   useEffect(() => {
     localStorage.setItem('inputDetailItems', JSON.stringify(inputArr));
   }, [inputArr]);
+
+  function handleModalOpen(){
+    setIsModalOpen(true);
+  }
 
   function handlechange(e) {
     const name = e.target.name;
@@ -65,8 +69,10 @@ function Todo() {
 
   return (
     <div className='todo_main'>
-      <form onSubmit={handleSubmit}>
+      <button className='todoAddBtn' onClick={handleModalOpen}>Add ToDo</button>
+    {isModalOpen? <form onSubmit={handleSubmit} className='form_detail'>
         <h1>Todo List</h1>
+        <i class="fa-solid fa-x" onClick={()=> setIsModalOpen(false)}></i>
         <label>*Enter Todo</label>
         <input type="text" placeholder='Enter Todo' name="todoname" value={inputs.todoname || ""} onChange={handlechange} required autoComplete='off' /> <br />
         <label>*Enter Todo Description</label>
@@ -78,7 +84,9 @@ function Todo() {
           <option value="lightGreen" >Light Green</option>
         </select> <br /> <br />
         {toggleEdit ? <button className='editBtn'>Edit</button> : <button>Submit </button>}
-      </form> <br />
+      </form> 
+      : null
+}
 
       <div style={{ marginTop: "2rem" }} className='todo_box_container'>
         {inputArr.map((item, index) => {
@@ -87,8 +95,8 @@ function Todo() {
               <h2>{item.todoname}</h2>
               <p><span> Todo Despription : </span>{item.todoDescription}</p>
               <div className='todo_time'>
-              <p><span> This Todo was Created on: </span> {item.currentdate} at {item.currenttime}</p>
-              {item.updatedtime && <p><span> This Todo was Updated on: </span> {item.updateddate} at {item.updatedtime}</p>}
+                <p><span> This Todo was Created on: </span> {item.currentdate} at {item.currenttime}</p>
+                {item.updatedtime && <p><span> This Todo was Updated on: </span> {item.updateddate} at {item.updatedtime}</p>}
               </div>
               <div className='btnContainer'>
                 <p><button onClick={() => { handleDelete(index) }}><i class="fa-solid fa-trash-can"></i></button></p>
